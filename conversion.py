4dd7d040bad1badbeb701ef6b0f7468da4c96a72
J-Chapman-one <157292251+J-Chapman-one@users.noreply.github.com>
2024-01-21 18:54:12 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,23 @@
+#Declare the following variables:
+# num1=99.23
+# num2=23
+# num3=150
+# string1=“100”
+
+# covert num 1 to a whole number type
+# covert num 2 to a decimal number type
+# covert num 3 to a word type
+# covert string 1 to a number type
+# after converting types, print above 4 variables on separate lines
+
+num1=99.23
+num2=23
+num3=150
+string1="100"
+
+number1=int(num1)
+number2=float(num2)
+number3=str(num3)
+string2=int(string1)
+
+print (number1, number2,number3,string2, sep="\n")
