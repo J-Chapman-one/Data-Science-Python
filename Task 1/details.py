0094bc3ec589c23ab24ba4aabcc595ca41d8e574
J-Chapman-one <157292251+J-Chapman-one@users.noreply.github.com>
2024-01-21 19:04:53 +0000

    Add files via upload

---
+++ Task 1/details.py
@@ -0,0 +1,11 @@
+#request input from the user on their Name, Age, Housenumber and Streetname
+#store input into separate variables called "name", "age", "housenumber", and "streetname" respectively
+#Print out a singles entence containing all the details of the user
+
+name = input("What is your name? ")
+age = (input("What is your age? "))
+housenumber = (input("What is your house number? "))
+streetname = input("What is your street name? ")
+
+print ("This is " + name + ". He is " + age + " years old and lives at house number " + housenumber + " on " + streetname + ".")
+                                                                            
